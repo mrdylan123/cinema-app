119df2765fdf71ef2fc02a27565e716243d3e52d
Dylan Martens <dylanhs123@hotmail.com>
2017-03-08 09:36:17 +0100

    Filter

---
+++ CInema.IVH7B4.UnitTests/UnitTest1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Cinema.IVH7B4.Domain.Abstract;
+using Cinema.IVH7B4.Domain.Entities;
+using Cinema.IVH7B4.WebUI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CInema.IVH7B4.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRequests()
+        {
+            var request = new Mock<HttpRequestBase>();
+            request.Setup(r => r.HttpMethod).Returns("GET");
+
+            var httpContext = new Mock<HttpContextBase>();
+            var session = new Mock<HttpSessionStateBase>();
+
+            httpContext.Setup(c => c.Request).Returns(request.Object);
+            httpContext.Setup(c => c.Session).Returns(session.Object);
+
+            session.Setup(c => c.Add("test", "something here"));
+
+            Mock<IFilmRepository> mock1 = new Mock<IFilmRepository>();
+
+            Mock<IShowingRepository> mock2 = new Mock<IShowingRepository>();
+
+            var controller = new FilterController(mock1.Object, mock2.Object);
+
+            controller.ControllerContext = new ControllerContext(
+                new RequestContext(httpContext.Object, new RouteData()), controller);
+
+            session.VerifyAll(); // function is trying to add the desired item to the session in the constructor
+
+            //TODO: Add assertion   
+        }
+    }
+
+}
+
+
+/**
+
+    [TestMethod]
+    public void TestGetFilteredFilms()
+    {
+        //Arrange
+
+        // make a list to later fill with films
+        List<Film> filmList = new List<Film>();
+
+        // make a list to later fill with showings
+        List<Showing> showingList = new List<Showing>();
+
+        // first make a location to use in film
+        Location testLocation = new Location
+        {
+            City = "",
+            Country = "",
+            kidDiscount = 0.0m,
+            LocationID = 1,
+            Name = "",
+            Province = "",
+            Rooms = 1,
+            SeniorDiscount = 0.0m,
+            StudentDiscount = 0.0m,
+            TicketPriceLong = 0.0m,
+            TicketPriceNormal = 0.0m,
+        };
+
+        Mock<IFilmRepository> mock1 = new Mock<IFilmRepository>();
+        mock1.Setup(f => f.GetFilms()).Returns(filmList = new List<Film> 
+            {
+                new Film
+                {
+                    Age = 1,
+                    Description = "",
+                    FilmID = 1,
+                    FilmType = 1,
+                    Image = null,
+                    Is3D = true,
+                    Language = "",
+                    LanguageSubs = "",
+                    Length = 1,
+                    Location = testLocation,
+                    LocationID = 1,
+                    Name = "",
+                },
+                new Film
+                {
+                    Age = 1,
+                    Description = "",
+                    FilmID = 2,
+                    FilmType = 1,
+                    Image = null,
+                    Is3D = false,
+                    Language = "",
+                    LanguageSubs = "",
+                    Length = 1,
+                    Location = testLocation,
+                    LocationID = 1,
+                    Name = "",
+                },
+                new Film
+                {
+                    Age = 1,
+                    Description = "",
+                    FilmID = 3,
+                    FilmType = 1,
+                    Image = null,
+                    Is3D = false,
+                    Language = "",
+                    LanguageSubs = "",
+                    Length = 1,
+                    Location = testLocation,
+                    LocationID = 1,
+                    Name = ""
+                }
+            }
+        );
+
+        // now create a roomlayout to add to room to add to showing
+        RoomLayout testLayout = new RoomLayout
+        {
+            BackX = 1,
+            BackY = 1,
+            FrontX = 1,
+            FrontY = 1,
+            LayoutID = 1
+        };
+
+        // now create a room that's using above layout
+        Room testRoom = new Room
+        {
+            Layout = testLayout,
+            LayoutID = 1,
+            Location = testLocation,
+            LocationID = 1,
+            RoomID = 1,
+            RoomNumber = 1
+        };
+
+        // create a list of showings
+        Mock<IShowingRepository> mock2 = new Mock<IShowingRepository>();
+        mock2.Setup(s => s.GetShowings()).Returns(new Showing[]
+        {
+            // include a showing in the past to test if this one is filtered out
+            new Showing
+            {
+                BeginDateTime = new DateTime(2017, 2, 15, 17, 00, 00),
+                EndDateTime = new DateTime(2017, 2, 15, 19, 00, 00),
+                Film = filmList[0],
+                FilmID = filmList[0].FilmID,
+                Room = testRoom,
+                RoomID = testRoom.RoomID,
+                ShowingID = 1
+            },
+            new Showing
+            {
+                BeginDateTime = new DateTime(2017, 3, 9, 17, 00, 00),
+                EndDateTime = new DateTime(2017, 3, 9, 19, 00, 00),
+                Film = filmList[0],
+                FilmID = filmList[0].FilmID,
+                Room = testRoom,
+                RoomID = testRoom.RoomID,
+                ShowingID = 2
+            },
+            new Showing
+            {
+                BeginDateTime = new DateTime(2017, 4, 21, 17, 00, 00),
+                EndDateTime = new DateTime(2017, 4, 21, 19, 00, 00),
+                Film = filmList[1],
+                FilmID = filmList[1].FilmID,
+                Room = testRoom,
+                RoomID = testRoom.RoomID,
+                ShowingID = 3
+            }
+        });
+
+        // create the controller class that will be tested
+        FilterController controller = new FilterController(mock1.Object, mock2.Object);
+
+        // create a datetime object that will be used as a chosen filter
+        DateTime selection = new DateTime(2017, 3, 11, 00, 00, 00);
+
+        // act - call the method from the controller
+        controller.GetFilteredFilms();
+        ControllerContext context = new ControllerContext();
+
+        context.HttpContext.Session.
+        int resultAmount = showingList.Count;
+        int expectedAmount = 1;
+
+        // assert - see if the resultamount (amount of showings remaining in showingList)
+        //          is equal to the expectedamount of 1
+        Assert.AreEqual(resultAmount, expectedAmount);
+    }
+}
+}
+**/
---
+++ Cinema.IVH7B4.WebUI/Controllers/FilterController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.IVH7B4.Domain.Abstract;
+using Cinema.IVH7B4.Domain.Entities;
+using Cinema.IVH7B4.WebUI.Models;
+using System.Globalization;
+
+namespace Cinema.IVH7B4.WebUI.Controllers
+{
+    public class FilterController : Controller
+    {
+        private IShowingRepository showingRepo;
+        private IFilmRepository filmRepo;
+
+        public FilterController(IFilmRepository filmRepo, IShowingRepository showingRepo)
+        {
+            this.filmRepo = filmRepo;
+            this.showingRepo = showingRepo;
+        }
+
+
+        public ActionResult GetFilteredFilms()
+        {
+            CinemaViewModel model = (CinemaViewModel)TempData["model"];
+
+            if (model == null)
+            {
+                model = new CinemaViewModel();
+            }
+
+
+            // Retrieve the user input
+            string dayInput = Request["searchDay"]; // format: 03-04-2017      (string)
+            string timeInput = Request["searchTime"]; // format: 13 14 15 .... (int)
+
+            // Initialize time variable that will be used to work with the user input
+            DateTime time;
+
+            // If a day has been entered, put this in time. Else, take the current day.
+            if (dayInput != "")
+            {
+                time = DateTime.ParseExact(dayInput, "dd-mm-yyyy", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                time = DateTime.Now;
+            }
+
+            // If a time has been entered, put this in time.
+            if (timeInput != "")
+            {
+                time = time.AddHours(Int32.Parse(timeInput));
+            }
+
+            /*
+            // split the string by the dash
+            string[] ints = dayInput.Split('-');
+
+            // create int variables
+            int y = 0;
+            int d = 0;
+            int m = 0;
+
+            // parse the strings from the searchDay result to ints
+            foreach (string s in ints)
+            {
+                int day = Int32.Parse(ints[0]);
+                int month = Int32.Parse(ints[1]);
+                d = day;
+                m = month;
+            }
+
+            // parse the string from the searchTime result to int
+            int chosentime = Int32.Parse(timeInput);
+
+            // create a datetime object of the chosen filter parameters
+            DateTime time = new DateTime(DateTime.Now.Year, m, d, chosentime, 0, 0);
+            */
+
+            var dayofweek = time.DayOfWeek;
+
+            //First get all showings
+            IEnumerable<Showing> allShowings = showingRepo.GetShowings();
+
+            //Filter the list to only contain showings coming after the given time
+            IEnumerable<Showing> filteredShowings = allShowings.Where(s => s.BeginDateTime > time);
+
+            //Order the results by date so that the most near one will be shown first
+            IEnumerable<Showing> orderedShowings = filteredShowings.OrderBy(s => s.BeginDateTime);
+            
+            //Filter out showings that are not on the chosen day
+            IEnumerable<Showing> todaysShowings = orderedShowings.Where(s => s.BeginDateTime.DayOfWeek == dayofweek);
+
+            //Take the first 10 showings to show on the page 
+            List<Showing> resultShowings = todaysShowings.Take(10).ToList();
+
+            //Add the corresponding film to the list of showings,
+            //so that the information of the film can be used
+            List<Film> allFilms = filmRepo.GetFilms().ToList();
+
+            //Add every film that has been found in the result list of showings
+            foreach (Showing s in resultShowings)
+            {
+                allFilms.Add(s.Film);
+            }
+
+            if (time.Day == DateTime.Now.Day)
+            {
+                ViewBag.selectedDate = "Vandaag";
+            }
+            else
+            {
+                ViewBag.selectedDate = time.DayOfWeek + " " + time.Day + " " + time.Month;
+            }
+
+            ViewBag.selectedTime = time.TimeOfDay.ToString("HH:mm");
+            ViewBag.numberOfResults = resultShowings.Count;
+            ViewBag.resultShowings = resultShowings;
+            ViewBag.resultFilms = allFilms;
+
+            TempData["Model"] = model;
+            return View("FilteredFilms", model);
+        }
+    }
+}
