0f727040df802c6c6b71fc81b7ac211ff5fe5822
Maikel van den Hout <mjwghout@avans.nl>
2017-03-13 13:38:52 +0100

    Add route map

---
+++ Cinema.IVH7B4.WebUI/Controllers/RouteMapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.IVH7B4.Domain.Concrete;
+
+namespace Cinema.IVH7B4.WebUI.Controllers
+{
+    public class RouteMapController : Controller
+    {
+        // GET: RouteMap
+        public ActionResult RouteMap()
+        {
+            ViewBag.Locations = new EFDbContext().Locations.ToList();
+            return View();
+        }
+    }
+}
