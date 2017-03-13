9b85a22c27bd2b7c8916d18f1e2f9ca94034b2eb
Maikel van den Hout <mjwghout@avans.nl>
2017-03-13 15:19:33 +0100

    Add newsletter support

---
+++ Cinema.IVH7B4.Domain/Entities/NewsletterRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinema.IVH7B4.Domain.Entities
+{
+    [Table("NewsletterRegistration")]
+    public class NewsletterRegistration
+    {
+        [Key]
+        public int RegistrationID { get; set; }
+
+        public int Age { get; set; }
+        public String Name { get; set; }
+        public String Location { get; set; }
+        public String Interests { get; set; }
+        public String Email { get; set; }
+
+    }
+}
---
+++ Cinema.IVH7B4.WebUI/Controllers/NewsletterController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.IVH7B4.Domain.Concrete;
+using Cinema.IVH7B4.Domain.Entities;
+
+namespace Cinema.IVH7B4.WebUI.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        // GET: Newsletter
+        public ActionResult ShowNewsletter()
+        {
+            return View("ShowNewsletter");
+        }
+
+        [HttpPost]
+        public ActionResult HandleSubmitForm()
+        {
+            String name = Request["Name"];
+            int age = Int32.Parse(Request["Age"]);
+            String location = Request["Location"];
+            String interests = Request["Interests"];
+            String email = Request["Email"];
+
+            var context = new EFDbContext();
+
+            if (context.NewsletterRegistrations.ToList().Find(nr => nr.Email == email) == null)
+            {
+                context.NewsletterRegistrations.Add(new NewsletterRegistration()
+                {
+                    Age = age,
+                    Email = email,
+                    Interests = interests,
+                    Name = name,
+                    Location = location
+                });
+
+                context.SaveChanges();
+                return View("ShowRegistrationThanks");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Email bestaat al");
+                return View("ShowNewsletter");
+            }
+            
+        }
+    }
+}
