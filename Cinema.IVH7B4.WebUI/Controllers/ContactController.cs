7c33884f106b2c109747d4e749dba220bcf4670c
Erik <frfkoole@avans.nl>
2017-03-15 14:33:04 +0100

    feedback verwerkt van contact pagina

--- Cinema.IVH7B4.WebUI/Controllers/ContactController.cs
+++ Cinema.IVH7B4.WebUI/Controllers/ContactController.cs
@@ -16,17 +16,62 @@ namespace Cinema.IVH7B4.WebUI.Controllers
         {
             CinemaViewModel model = (CinemaViewModel)TempData["model"];
 
+            ViewBag.Location = "Breda";
+
             TempData["model"] = model;
             return View("Contact", model);
+        }
+
+        public ActionResult SetLocationAndReturn(String locationName)
+        {
+            CinemaViewModel model = (CinemaViewModel)TempData["model"];
+            if (locationName != null && model != null)
+            {
+                model.SelectedLocation = model.findLocationByName(locationName);
+            }
+            else if (model == null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                model.SelectedLocation = model.findLocationByName("Breda");
+            }
+
+            TempData["model"] = model;
+            return RedirectToAction("Contact", "Contact");
         }
 
         [HttpPost]
         public ActionResult HandleSubmitForm()
         {
+            String interest = "";
+            Dictionary<string,string> checkboxlist = new Dictionary<string, string>();
+            checkboxlist.Add("Actie",Request["Actie"]);
+            checkboxlist.Add("Drama", Request["Drama"]);
+            checkboxlist.Add("Avontuur", Request["Avontuur"]);
+            checkboxlist.Add("Thriller", Request["Thriller"]);
+            checkboxlist.Add("Animatie", Request["Animatie"]);
+            checkboxlist.Add("Horror", Request["Horror"]);
+            checkboxlist.Add("Komedie", Request["Komedie"]);
+            checkboxlist.Add("Romantiek", Request["Romantiek"]);
+
+            for(int i = 0; i< checkboxlist.Count; i++)
+            {
+                if (checkboxlist.ElementAt(i).Value != null)
+                {
+                   interest += checkboxlist.ElementAt(i).Key + ",";
+                }
+                else
+                {
+                    interest += "";
+                }
+            }
+
             String name = Request["Name"];
             int age = Int32.Parse(Request["Age"]);
             String location = Request["Location"];
-            String interests = Request["Interests"];
+            String interests = interest;
             String email = Request["Email"];
 
             var context = new EFDbContext();
